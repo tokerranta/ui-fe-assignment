b4e813378d746140ba7ae29cc1b9404fb69c2024
markus rantanikunen <nomarran@postkodlotteriet.se>
2025-06-30 08:21:55 +0200

    add filters

--- app/devices/devices.tsx
+++ app/devices/devices.tsx
@@ -1,16 +1,51 @@
 'use client';
 import { Device } from '@/api/devices-schema';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { Autocomplete } from '@/app/components';
 import { DevicesResult } from './devices-result';
 import Link from 'next/link';
 import GridViewIcon from './grid-view.svg';
 import ListViewIcon from './list-view.svg';
 
-const Tools = () => {
+type ToolsProps = {
+  devices: Array<Device>;
+};
+
+const Tools = ({ devices }: ToolsProps) => {
   const searchParams = useSearchParams();
+  const router = useRouter();
+  const deviceFilters = Array.from(
+    new Set(
+      devices
+        .map((devices) => devices.line.name)
+        .toSorted((a, b) => a.localeCompare(b))
+    )
+  );
+  const [showFilter, setShowFilter] = React.useState(false);
+  const hasNoFilters = searchParams.has('filter') === false;
+  const filterParam = searchParams.get('filter');
+  const mappedFilter = filterParam?.split(',').map((f) => f.trim()) ?? [];
   const displayMode = searchParams.get('display') ?? 'list';
+
+  const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const filterValue = e.target.value;
+    const newFilters = e.target.checked
+      ? [...mappedFilter, filterValue]
+      : mappedFilter.filter((f) => f !== filterValue);
+
+    if (newFilters.length === 0) {
+      router.push(`/devices?display=${displayMode}`);
+      return;
+    }
+
+    router.push(
+      `/devices?filter=${newFilters.join(',')}&display=${displayMode}`
+    );
+  };
+
+  const handleToggleFilter = () => setShowFilter((prev) => !prev);
+
   return (
     <div className="flex items-center gap-2 px-4">
       <Link href={`/devices?display=list`}>
@@ -27,6 +62,47 @@ const Tools = () => {
           }`}
         />
       </Link>
+      <div className="relative">
+        <button
+          onClick={handleToggleFilter}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Filter
+        </button>
+        <div
+          className={`absolute z-50 p-4 right-0 mt-2 w-48 bg-white rounded shadow-lg flex flex-col gap-4 ${
+            showFilter ? '' : 'hidden'
+          }`}
+        >
+          <h1 className="font-bold">Product Line</h1>
+          <div className={`flex flex-col gap-2`}>
+            {deviceFilters.map((filter) => (
+              <div key={filter} className="flex items-center gap-2">
+                <input
+                  type="checkbox"
+                  onChange={handleFilterChange}
+                  id={`line-${filter}`}
+                  value={filter}
+                  checked={mappedFilter.includes(filter)}
+                  className="form-checkbox hover:cursor-pointer"
+                />
+                <label
+                  htmlFor={`line-${filter}`}
+                  className="text-sm hover:cursor-pointer"
+                >
+                  {filter}
+                </label>
+              </div>
+            ))}
+          </div>
+          <Link
+            href={`/devices?display=${displayMode}`}
+            className={`${hasNoFilters ? 'text-red-300' : 'text-red-400'}`}
+          >
+            Reset
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
@@ -66,7 +142,7 @@ export const Devices = ({ devices }: DevicesProps) => {
             value: d.id,
           }))}
         />
-        <Tools />
+        <Tools devices={devices} />
       </div>
       <DevicesResult devices={filteredDevices} />
     </div>
